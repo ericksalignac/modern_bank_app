ffde208dd6b5aec0808d893c3ff9816a22352d2b
ericksalignac <ericksalignac@gmail.com>
2022-08-16 16:28:57 -0300

    Components structure

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+import {close, logo, menu} from '../assets';
+import {navLinks} from '../constants';
+
+const NavBar = () => {
+  return (
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+       <img src={logo} alt="hoobank" className='w-[124px] h-[32px]'/>
+       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {/* {navLinks.map((nav, index) => (
+
+        ))} */}
+       </ul>
+    </nav>
+  )
+}
+
+export default NavBar
